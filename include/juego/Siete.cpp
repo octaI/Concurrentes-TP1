d0fe416ba2890fb4d7ea2d71df1a0b6318011665
juanmanuelbouvier <juanmabouvier@gmail.com>
2017-04-06 00:00:56 -0300

    Agrego nueva carta especial  Siete (o sea, que hereda comportamiento de Carta)

---
+++ include/juego/Siete.cpp
@@ -0,0 +1,4 @@
+
+#include "Siete.h"
+
+Siete::Siete(Palo palo) : Carta(7, palo) {}
---
+++ include/juego/Siete.h
@@ -0,0 +1,16 @@
+
+#ifndef CONCURRENTES_TP1_SIETE_H
+#define CONCURRENTES_TP1_SIETE_H
+
+
+#include "Carta.h"
+
+class Siete : Carta {
+
+public:
+    Siete(Palo palo);
+
+};
+
+
+#endif //CONCURRENTES_TP1_SIETE_H

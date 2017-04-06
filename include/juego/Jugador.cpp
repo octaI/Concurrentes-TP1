a137264bca016884408a911d80f6a10ac230b9dc
juanmanuelbouvier <juanmabouvier@gmail.com>
2017-04-05 21:27:43 -0300

    Clase Jugador vacia

---
+++ include/juego/Jugador.cpp
@@ -0,0 +1,1 @@
+#include "../../src/juego/Jugador.h"
---
+++ src/juego/Jugador.h
@@ -0,0 +1,20 @@
+
+#ifndef CONCURRENTES_TP1_JUGADOR_H
+#define CONCURRENTES_TP1_JUGADOR_H
+
+
+class Jugador {
+
+private:
+
+
+public:
+    Jugador();
+    ~Jugador();
+    Jugador(const Jugador& origen);
+    Jugador& operator= (const Jugador& origen);
+
+};
+
+
+#endif //CONCURRENTES_TP1_JUGADOR_H

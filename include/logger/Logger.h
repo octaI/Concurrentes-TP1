04c7058a94acad7344729c5abab367fc769bdbbd
juanmanuelbouvier <juanmabouvier@gmail.com>
2017-04-22 20:58:35 -0300

    Agrego logger (sin chequeo de errores aun)

--- include/logger/Logger.h
+++ include/logger/Logger.h
@@ -2,10 +2,39 @@
 #ifndef CONCURRENTES_TP1_LOGGER_H
 #define CONCURRENTES_TP1_LOGGER_H
 
+#include <iostream>
+#include <stdio.h>
+#include <ctime>
+
+#include "../../include/ipc/LockFile.h"
+
+using namespace std;
 
 class Logger {
+
+private:
+    static const string ARCHIVO_BITACORA;
+
+    static const string DEBUG;
+    static const string INFO;
+    static const string WARN;
+    static const string ERROR;
+
+    static Logger* instance;
+    Logger ( void );
+
+    int escribir ( string modo, string msg );
+    string fechaActual ();
+
 public:
-    static void log();
+    static Logger* getInstance ();
+
+    int debug(string msg);
+    int info(string msg);
+    int warn(string msg);
+    int error(string msg);
+
+
 };
 
 
--- src/logger/Logger.cpp
+++ src/logger/Logger.cpp
@@ -1,6 +1,62 @@
 
 #include "../../include/logger/Logger.h"
 
-void Logger::log() {
-    // TODO: Implement.
+const string Logger::ARCHIVO_BITACORA = "../../logs/log.txt";
+const string Logger::DEBUG = "debug";
+const string Logger::INFO = "info";
+const string Logger::WARN = "warn";
+const string Logger::ERROR = "error";
+
+Logger* Logger :: instance = NULL;
+
+Logger::Logger() {}
+
+Logger* Logger::getInstance() {
+
+    if ( instance == NULL ) {
+        instance = new Logger();
+    }
+    return instance;
+
+}
+
+int Logger::escribir ( string modo, string msg ) {
+    LockFile lock (ARCHIVO_BITACORA);
+    lock.tomarLock();
+
+    const string log = fechaActual() + ": [" + modo + "] " + msg;
+    int resultado = lock.escribir ( static_cast<const void *>(log.c_str()), sizeof(log) );
+
+    lock.liberarLock();
+    delete &lock;
+    return resultado;
+}
+
+// TODO: Buscar metodo para trabajar con precision de milisegundos, sino el log pierde sentido.
+string Logger::fechaActual() {
+    time_t t = time(0);
+    struct tm * now = localtime( & t );
+    return to_string(now->tm_year + 1900) + '-'
+           + to_string(now->tm_mon + 1) + '-'
+           + to_string(now->tm_mday)
+           + to_string(now->tm_mday)
+           + to_string(now->tm_hour)
+           + to_string(now->tm_min)
+           + to_string(now->tm_sec);
+}
+
+int Logger::debug(string msg) {
+    return escribir(DEBUG, msg);
+}
+
+int Logger::info(string msg) {
+    return escribir(INFO, msg);
+}
+
+int Logger::warn(string msg) {
+    return escribir(WARN, msg);
+}
+
+int Logger::error(string msg) {
+    return escribir(ERROR, msg);
 }

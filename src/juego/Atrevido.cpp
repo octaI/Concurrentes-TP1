df25ee4270f969e0152fbb90b8d2da0031a23246
juanmanuelbouvier <juanmabouvier@gmail.com>
2017-04-30 15:42:58 -0300

    Borro codigo comentado

--- src/juego/Atrevido.cpp
+++ src/juego/Atrevido.cpp
@@ -12,14 +12,12 @@ void Atrevido::iniciarJugadores(const int nroJugadores) {
     Logger::getInstance() -> info ( "Atrevido.cpp", "Van a jugar " + to_string(nroJugadores) + " jugadores" );
 
     int valoresInicialesJugadores [nroJugadores];
-    int valorInicialCantjugadores[1] = {nroJugadores};
     std::fill_n(valoresInicialesJugadores, nroJugadores, 0);    // inicializados en 0
     valoresInicialesJugadores [0] = 1;  // empieza el primer jugador
     Semaforo semaforosJugadores ( "Atrevido.cpp", 'j', valoresInicialesJugadores, nroJugadores );
 
     Logger :: getInstance() -> debug( "Atrevido.cpp", "Soy el PADRE Atrevido con PID: " + to_string(getpid()));
 
-    //Semaforo semaforoAtrevido("Atrevido.cpp", 'a',valorInicialCantjugadores, 1);
     int i;
     pid_t pid;
     Mazo* mazo = new Mazo ();
@@ -35,13 +33,11 @@ void Atrevido::iniciarJugadores(const int nroJugadores) {
                     + " (padre: " + to_string(getppid()) + ")" );
             jugador = new Jugador ( i + 1, nroJugadores, &semaforosJugadores );
             repartirCartas(mazo, jugador, nroJugadores);
-            //semaforoAtrevido.wait(0,1);
             break;
         }
     }
 
     if ( pid == 0 ) {
-        //semaforoAtrevido.barrier(0);
         jugador->jugar();
     } else {
         wait(NULL);

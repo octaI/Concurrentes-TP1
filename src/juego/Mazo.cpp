0b3e433728efda96c3073a31b5a4ae1f6bc82295
juanmanuelbouvier <juanmabouvier@gmail.com>
2017-05-03 20:54:43 -0300

    Uso cartas especiales (hijas de Carta) en la generacion del mazo

--- src/juego/Mazo.cpp
+++ src/juego/Mazo.cpp
@@ -17,16 +17,16 @@ void Mazo::generar() {
             indice++;
         }
 
-        Carta* siete = new Carta(7, Palo(palo));
+        Carta* siete = new Siete(Palo(palo));
         cartas->push(siete);
 
-        Carta *sota = new Carta(10, Palo(palo));
+        Carta *sota = new Sota(Palo(palo));
         cartas->push(sota);
 
-        Carta *caballo = new Carta(11, Palo(palo));
+        Carta *caballo = new Caballo(Palo(palo));
         cartas->push(caballo);
 
-        Carta *rey = new Carta(12, Palo(palo));
+        Carta *rey = new Rey(Palo(palo));
         cartas->push(rey);
     }
 
